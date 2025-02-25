9f6c237517b2dd015bbe9767569785c6f95ed7a9
Elena Ketcheson <elenaketcheson@Elenas-Air-2.app.byu.edu>
2025-02-25 16:47:10 -0700

    Added routing configurations

--- src/app.jsx
+++ src/app.jsx
@@ -1,33 +1,47 @@
 import React from 'react';
 import './app.css';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Shop } from './shop/shop';
+import { Enclosure } from './enclosure/enclosure';
 
 export default function App() {
     return (
-        <div className="body bg-dark text-light">
-            <header>
-                    <h1>Dinosaur Pals</h1>
-                    <nav>
-                        <menu>
-                            <li><a href="index.html">Home</a></li>
-                            <li><a href="enclosure.html">My Enclosure</a></li>
-                            <li><a href="shop.html">Shop</a></li>
-                        </menu>
-                    </nav>
+        <BrowserRouter>
+            <div className="body bg-dark text-light">
+                <header>
+                        <h1>Dinosaur Pals</h1>
+                        <nav>
+                            <menu>
+                                <li><NavLink className='nav-link' to=''>Login</NavLink></li>
+                                <li><NavLink className='nav-link' to='enclosure'>My Enclosure</NavLink></li>
+                                <li><NavLink className='nav-link' to='shop'>Shop</NavLink></li>
+                            </menu>
+                        </nav>
 
-                </header>
+                    </header>
 
-                <main>
-                    App content goes here
-                </main>
-                <footer>
-                    Problems? Let us know!
-                    <br />
-                    elenakay@byu.edu
-                    <br />
-                    <span className="text-reset">Elena Ketcheson</span>
-                    <br />
-                    <a href="https://github.com/BananaProgram/startup">GitHub</a>
-                </footer>
-        </div>
+                    <Routes>
+                        <Route path='/' element={<Login />} exact />
+                        <Route path='/enclosure' element={<Enclosure />} />
+                        <Route path='/shop' element={<Shop />} />
+                        <Route path='*' element={<NotFound />} />
+                    </Routes>
+
+                    <footer>
+                        Problems? Let us know!
+                        <br />
+                        elenakay@byu.edu
+                        <br />
+                        <span className="text-reset">Elena Ketcheson</span>
+                        <br />
+                        <a href="https://github.com/BananaProgram/startup">GitHub</a>
+                    </footer>
+            </div>
+        </BrowserRouter>
     );
+}
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
 }
