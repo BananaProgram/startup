c2b5e1f3f0db07618cfa4ffefc63ccc6ca0211d9
Elena Ketcheson <elenaketcheson@Elenas-Air-2.app.byu.edu>
2025-02-25 16:59:56 -0700

    port jsx.jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 export function Login() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+      <div>
+        <h1>Welcome to Dinosaur Pals!</h1>
+        <h3>Curate your very own collection of dino friends</h3>
+            
+        <form method="get" action="enclosure.html">
+            <div>
+                <span>Email: </span>
+                <input type="text" placeholder="Email address" />
+            </div>
+            <div>
+                <span>Password: </span>
+                <input type="password" placeholder="Password" />
+            </div>
+            <button type="submit">Login</button>
+            <button type="submit">Create Account</button>
+        </form>
+
+      </div>
     </main>
   );
 }
