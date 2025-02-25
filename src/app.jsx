9598a7947cc4fa91c57d62cd0bbbb27f40a6974c
Elena Ketcheson <elenaketcheson@Elenas-Air-2.app.byu.edu>
2025-02-25 15:14:35 -0700

    updated app.jsx to contain header and footer code

--- src/app.jsx
+++ src/app.jsx
@@ -2,5 +2,32 @@ import React from 'react';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+    return (
+        <div className="body bg-dark text-light">
+            <header>
+                    <h1>Dinosaur Pals</h1>
+                    <nav>
+                        <menu>
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="enclosure.html">My Enclosure</a></li>
+                            <li><a href="shop.html">Shop</a></li>
+                        </menu>
+                    </nav>
+
+                </header>
+
+                <main>
+                    App content goes here
+                </main>
+                <footer>
+                    Problems? Let us know!
+                    <br />
+                    elenakay@byu.edu
+                    <br />
+                    <span className="text-reset">Elena Ketcheson</span>
+                    <br />
+                    <a href="https://github.com/BananaProgram/startup">GitHub</a>
+                </footer>
+        </div>
+    );
 }
