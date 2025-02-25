f208d758be4232bf47942eda072c11a75c10a753
Elena Ketcheson <elenaketcheson@Elenas-Air-2.app.byu.edu>
2025-02-25 16:32:05 -0700

    created component jsx files

---
+++ src/enclosure/enclosure.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Enclosure() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>enclosure displayed here</div>
+    </main>
+  );
+}
