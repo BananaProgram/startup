bd8ab7213b34d9ecdc7ce0559a4c96d8d9f415a6
Elena Ketcheson <elenaketcheson@Elenas-Air-2.app.byu.edu>
2025-02-25 17:54:09 -0700

    updated image filepaths

--- src/enclosure/enclosure.jsx
+++ src/enclosure/enclosure.jsx
@@ -4,90 +4,90 @@ import './enclosure.css';
 export function Enclosure() {
   return (
     <main className="container-fluid bg-secondary text-center">
-      <div id="title-menu">
-                <div className="title-menu" id="balances">
-                    <div className="balances" id="scales">
-                        <img src="dragon-scales.png"/><p className="balance-labels">123</p>
-                    </div>
-                    <div className="balances" id="food">
-                        <img src="food.png"/><p className="balance-labels">123</p>
-                    </div>
+        <div id="title-menu">
+            <div className="title-menu" id="balances">
+                <div className="balances" id="scales">
+                    <img src="/public/dragon-scales.png"/><p className="balance-labels">123</p>
                 </div>
-                <div className="title-menu">
-                    <h1>user_name's Dino Enclosure</h1>
-                </div>
-                <div className="title-menu" id="find-friends">
-                    <input type="text" placeholder="Find friends" />
+                <div className="balances" id="food">
+                    <img src="/public/food.png"/><p className="balance-labels">123</p>
                 </div>
             </div>
-            
-            <div id="dino-grid">
-                <div>
-                    <h4>Type of Dinosaur</h4>
-                    <div>
-                        <img src="dino.png"/>
-                    </div>
-                    <div>
-                        Health <meter min="0" max="100" value="68" ></meter>
-                    </div>
-                    <div>
-                        Happiness <meter min="0" max="100" value="88" ></meter>
-                    </div>
-                    <button>Feed me!</button>
-                </div>
-                <div>
-                    <h4>Type of Dinosaur</h4>
-                    <div>
-                        <img src="dino.png"/>
-                    </div>
-                    <div>
-                        Health <meter min="0" max="100" value="68" ></meter>
-                    </div>
-                    <div>
-                        Happiness <meter min="0" max="100" value="88" ></meter>
-                    </div>
-                    <button>Feed me!</button>
-                </div>
-                <div>
-                    <h4>Type of Dinosaur</h4>
-                    <div>
-                        <img src="dino.png"/>
-                    </div>
-                    <div>
-                        Health <meter min="0" max="100" value="68" ></meter>
-                    </div>
-                    <div>
-                        Happiness <meter min="0" max="100" value="88" ></meter>
-                    </div>
-                    <button>Feed me!</button>
-                </div>
-                <div>
-                    <h4>Type of Dinosaur</h4>
-                    <div>
-                        <img src="dino.png"/>
-                    </div>
-                    <div>
-                        Health <meter min="0" max="100" value="68" ></meter>
-                    </div>
-                    <div>
-                        Happiness <meter min="0" max="100" value="88" ></meter>
-                    </div>
-                    <button>Feed me!</button>
-                </div>
-                <div>
-                    <h4>Type of Dinosaur</h4>
-                    <div>
-                        <img src="dino.png"/>
-                    </div>
-                    <div>
-                        Health <meter min="0" max="100" value="68" ></meter>
-                    </div>
-                    <div>
-                        Happiness <meter min="0" max="100" value="88" ></meter>
-                    </div>
-                    <button>Feed me!</button>
-                </div>
+            <div className="title-menu">
+                <h1>user_name's Dino Enclosure</h1>
             </div>
+            <div className="title-menu" id="find-friends">
+                <input type="text" placeholder="Find friends" />
+            </div>
+        </div>
+        
+        <div id="dino-grid">
+            <div>
+                <h4>Type of Dinosaur</h4>
+                <div>
+                    <img src="/public/dino.png"/>
+                </div>
+                <div>
+                    Health <meter min="0" max="100" value="68" ></meter>
+                </div>
+                <div>
+                    Happiness <meter min="0" max="100" value="88" ></meter>
+                </div>
+                <button>Feed me!</button>
+            </div>
+            <div>
+                <h4>Type of Dinosaur</h4>
+                <div>
+                    <img src="/public/dino.png"/>
+                </div>
+                <div>
+                    Health <meter min="0" max="100" value="68" ></meter>
+                </div>
+                <div>
+                    Happiness <meter min="0" max="100" value="88" ></meter>
+                </div>
+                <button>Feed me!</button>
+            </div>
+            <div>
+                <h4>Type of Dinosaur</h4>
+                <div>
+                    <img src="/public/dino.png"/>
+                </div>
+                <div>
+                    Health <meter min="0" max="100" value="68" ></meter>
+                </div>
+                <div>
+                    Happiness <meter min="0" max="100" value="88" ></meter>
+                </div>
+                <button>Feed me!</button>
+            </div>
+            <div>
+                <h4>Type of Dinosaur</h4>
+                <div>
+                    <img src="/public/dino.png"/>
+                </div>
+                <div>
+                    Health <meter min="0" max="100" value="68" ></meter>
+                </div>
+                <div>
+                    Happiness <meter min="0" max="100" value="88" ></meter>
+                </div>
+                <button>Feed me!</button>
+            </div>
+            <div>
+                <h4>Type of Dinosaur</h4>
+                <div>
+                    <img src="/public/dino.png"/>
+                </div>
+                <div>
+                    Health <meter min="0" max="100" value="68" ></meter>
+                </div>
+                <div>
+                    Happiness <meter min="0" max="100" value="88" ></meter>
+                </div>
+                <button>Feed me!</button>
+            </div>
+        </div>
     </main>
   );
 }
