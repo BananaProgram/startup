ea2b5089e51a8dfa48a2640df68fd84474c4cfed
Elena Ketcheson <elenaketcheson@Elenas-Air-2.app.byu.edu>
2025-03-25 20:35:19 -0600

    Created database.js and added functions to create and find users

---
+++ service/database.js
@@ -0,0 +1,36 @@
+const { MongoClient } = require('mongodb');
+const config = require('./dbConfig.json');
+
+const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
+const client = new MongoClient(url);
+const db = client.db('startup');
+const users = db.collection('user');
+
+(async function testConnection() {
+  try {
+    await db.command({ ping: 1 });
+    console.log(`Connect to database`);
+  } catch (ex) {
+    console.log(`Unable to connect to database with ${url} because ${ex.message}`);
+    process.exit(1);
+  }
+})();
+
+function getUser(email) {
+  return users.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return users.findOne({ token: token });
+}
+
+async function addUser(user) {
+  await users.insertOne(user);
+}
+
+
+module.exports = {
+  getUser,
+  getUserByToken,
+  addUser
+};
